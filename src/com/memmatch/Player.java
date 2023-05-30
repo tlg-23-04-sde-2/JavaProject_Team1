1cdca41e4fce1217d881e8b68ff2350621f73bdc
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-05-30 15:01:41 -0400

    changed input logic

--- src/com/memmatch/Player.java
+++ src/com/memmatch/Player.java
@@ -13,18 +13,21 @@ public class Player {
         try {
             while (!validInput) {
                 System.out.println("Please select the card you would like to flip, the cards are listed 0 through " + (board.length - 1) + ":");
-                guess = scanner.nextInt();
-                if (guess < 0 || guess >= board.length) {
-                    System.out.println("Invalid guess, please enter a valid card number 0 through " + (board.length - 1) + ":");
+                String input = scanner.nextLine().trim();
+                if (input.matches("\\d")) {
+                    guess = Integer.parseInt(input);
+                    if (guess < 0 || guess >= board.length) {
+                        System.out.println("Invalid guess, please enter a valid card number 0 through " + (board.length - 1) + ":");
+                    }
+                    if (isFlipped[guess]) {
+                        System.out.println("The card you selected has already been flipped, please choose another card.");
+                    } else {
+                        validInput = true;
+                    }
                 }
-                if (isFlipped[guess]) {
-                    System.out.println("The card you selected has already been flipped, please choose another card.");
-                } else {
-                    validInput = true;
-                }
+                attempts++;
+                return guess;
             }
-            attempts++;
-            return guess;
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
